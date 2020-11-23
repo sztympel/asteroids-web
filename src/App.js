7fe0c1d29b2800c506f277536bc55fbfe24a5d5f
Mateusz Stępień <mateuszstepien@MacBook-Pro-Mateusz.local>
2020-11-23 12:34:21 +0100

    Display ship model

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
-import React from 'react';
+import React, { Suspense } from 'react';
 import { Canvas } from 'react-three-fiber';
 import { Ground } from './components/Ground';
+import { Ship } from './components/Ship';
 import { Stars } from '@react-three/drei';
 
 const App = () => {
   return (
     <Canvas style={{ background: 'black' }}>
+      <directionalLight position={[1, 1, 1]} intensity={1} />
+      <Suspense fallback={null}>
+        <Ship />
+      </Suspense>
       <Ground />
       <Stars />
     </Canvas>
